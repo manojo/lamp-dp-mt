d876bafc684de0006898a5ce7abeb71c50534cb9
Thierry Coppey <tcknetwork@hotmail.com>
2012-11-01 13:45:16 +0100

    JNI stub for DP.

--- src/test/jni/src/Toy.scala
+++ src/test/jni/src/Toy.scala
@@ -26,6 +26,25 @@ class Vec3(val x:Int, val y:Int, val z:Int) {
   @native def dot(v: Vec3):Vec3; //= new Vec3(x*v.x,y*v.y,z*v.z)
 }
 
+/** Dynamic Programming solver struct for CUDA */
+class CudaDP {
+  type Input = Array[Product];
+  type Backtrack = Array[Tuple2[Int,Int]];
+
+  Loader.load("CudaDP")
+  // Initialize the problem
+  @native def inArrays(in1: Input, in2: Input):Unit;
+  @native def inStrings(in1: String, in2: String):Unit;
+  // Compute the matrix content
+  @native def computeMatrix:Unit;
+  // Get score and backtrack
+  @native def getScore:Long;
+  @native def getBacktrack:Backtrack;
+  // Release structures
+  @native def free:Unit;
+  //@native override def finalize:Unit;
+}
+
 /** Demo application */
 object Toy {
   def main(args: Array[String]) {
@@ -33,5 +52,16 @@ object Toy {
     val b = new Vec3(3,2,1)
     val c = a.dot(b)
     System.out.println(c);
+
+    val dp = new CudaDP
+    dp.inStrings("Hello world","Hello Manohar")
+
+    type Input = Array[Tuple2[Int,Int]]
+
+    dp.inArrays(Array((1,3),(3,5),(5,9),(9,2),(2,4)),null)
+    dp.computeMatrix
+    println("Score = "+dp.getScore)
+    println("Backtrack = "+dp.getBacktrack)
+    dp.free
   }
 }
