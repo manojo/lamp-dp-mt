8d204e554e92aa482224e0f89a02e69d209c702c
Thierry Coppey <tcknetwork@hotmail.com>
2012-11-08 10:58:02 +0100

    Nits in test.

--- src/test/jni/src/Toy.scala
+++ src/test/jni/src/Toy.scala
@@ -27,8 +27,8 @@ class Vec3(val x:Int, val y:Int, val z:Int) {
 }
 
 /** Dynamic Programming solver struct for CUDA */
-class CudaDP {
-  type Input = Array[Product];
+class CudaDP[TI] {
+  type Input = Array[TI];
   type Backtrack = Array[Tuple2[Int,Int]];
 
   Loader.load("CudaDP")
@@ -47,21 +47,56 @@ class CudaDP {
 
 /** Demo application */
 object Toy {
+  type Input = (Int,Int)
+
+  // Input typing
+  lazy val inTypes:List[String] = {
+  	import scala.reflect.runtime.universe._
+  	var s = typeOf[Input].toString;
+  	s.substring(1,s.length-1).replace("scala.","").toLowerCase.split(", ").toList
+  }
+  lazy val struct = "typedef struct { "+inTypes.zipWithIndex.map{case (x,i)=>x+" _"+(i+1)}.mkString("; ")+"; } in_t;"
+  lazy val loader = {
+    val k = Map(("boolean","Z"),("byte","B"),("char","C"),("short","S"),("int","I"),("long","J"),("float","F"),("double","D"))
+    val ts = inTypes
+    val ms = ts.map {x=>k(x)}.zipWithIndex.map{case(t,i) => "    (*env)->GetMethodID(env, cls, \"_"+(i+1)+"\", \"()"+t+"\")," }
+    var es = ts.zipWithIndex.map{case(t,i) => "    e->_"+(i+1)+" = ("+t+") (*env)->CallObjectMethod(env, elem, ms["+i+"]);" }
+    "  jmethodID ms[] = {\n"+ms.mkString("\n")+"\n  };\n\n  for (i=0;i<size;++i) {\n"+
+    "    elem = (*env)->GetObjectArrayElement(env, input, i);\n"+
+    "    TI* e = &((*in)[i]);\n" +  es.mkString("\n")+"\n  }\n"
+  }
+  
+
   def main(args: Array[String]) {
+    println("C struct:")
+    println(struct);
+    println("C loader:")
+    println(loader);
+    println
+
     val a = new Vec3(1,2,3)
     val b = new Vec3(3,2,1)
     val c = a.dot(b)
     System.out.println(c);
 
-    val dp = new CudaDP
+    val dp = new CudaDP[Input]
     dp.inStrings("Hello world","Hello Manohar")
 
-    type Input = Array[Tuple2[Int,Int]]
-
-    dp.inArrays(Array((1,3),(3,5),(5,9),(9,2),(2,4)),null)
+    dp.inArrays( Array((1,3),(3,5),(5,9),(9,2),(2,4)) ,null)
     dp.computeMatrix
     println("Score = "+dp.getScore)
     println("Backtrack = "+dp.getBacktrack)
     dp.free
   }
 }
+
+  // get the C definition of this type from one element
+  /*
+  println(ctype( object))
+  def ctype(p:Product):String = {
+    def n(i:Int) = p.productElement(i).getClass.toString match {
+      case "class java.lang.Integer" => "int" case x => x // XXX: continue
+    }
+    "typedef struct { "+( (0 to p.productArity-1) map { x=>n(x) +" _"+(x+1)+";" } mkString(" ") )+" } in_t;"
+  }
+  */
