518aae020cd6dfb17d90ba1f5ec8f62200938b6c
Thierry Coppey <tcknetwork@hotmail.com>
2012-11-19 17:06:17 +0100

    Unrolling to earn 10% of the speed.

--- src/main/c/include/small_gpu.h
+++ src/main/c/include/small_gpu.h
@@ -13,10 +13,11 @@
 #define _in(i) in0[i%M_H]
 #endif
 
+#define _unroll _Pragma ("unroll 5") // Optimized by hand for my GPU
 #define _cost(i,j) cost[idx(i,j)]
-#define _max_loop(K,MIN,MAX,EXPR,BACK) for (unsigned K=MIN; K<MAX; ++K) { c2=EXPR; if (c2>c) { c=c2; b=BACK; } }
+#define _max_loop(K,MIN,MAX,EXPR,BACK) _unroll for (unsigned K=MIN; K<MAX; ++K) { c2=EXPR; if (c2>c) { c=c2; b=BACK; } }
 #define _max(EXPR,BACK) c2=EXPR; if (c2>=c) { c=c2; b=BACK; }
-#define _min_loop(K,MIN,MAX,EXPR,BACK) for (unsigned K=MIN; K<MAX; ++K) { c2=EXPR; if (c2<c) { c=c2; b=BACK; } }
+#define _min_loop(K,MIN,MAX,EXPR,BACK) _unroll for (unsigned K=MIN; K<MAX; ++K) { c2=EXPR; if (c2<c) { c=c2; b=BACK; } }
 #define _min(EXPR,BACK) c2=EXPR; if (c2<=c) { c=c2; b=BACK; }
 
 // -----------------------------------------------------------------------------
