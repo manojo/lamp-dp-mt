068cb89a4ebf7a50eaab42ffbb9524d39066eabd
Manojo <manojo10386@gmail.com>
2012-11-20 17:31:18 +0100

    HackyRangeOps

---
+++ src/main/scala/lms/HackyRangeOps.scala
@@ -0,0 +1,30 @@
+package lms
+
+import java.io.PrintWriter
+
+import scala.virtualization.lms.common._
+import scala.virtualization.lms.internal.{GenericNestedCodegen, GenerationFailedException}
+import scala.reflect.SourceContext
+
+trait HackyRangeOps extends RangeOps {
+  def infix_toList(r: Rep[Range])(implicit pos: SourceContext) = range_toList(r)
+  def range_toList(r: Rep[Range])(implicit pos: SourceContext) : Rep[List[Int]]
+}
+
+trait HackyRangeOpsExp extends HackyRangeOps with RangeOpsExp {
+
+  case class RangetoList(r: Exp[Range]) extends Def[List[Int]]
+
+  def range_toList(r: Rep[Range])(implicit pos: SourceContext) = RangetoList(r)
+
+}
+
+trait ScalaGenHackyRangeOps extends ScalaGenRangeOps {
+  val IR: HackyRangeOpsExp
+  import IR._
+
+  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+    case RangetoList(r) => emitValDef(sym, quote(r) + ".toList")
+    case _ => super.emitNode(sym, rhs)
+  }
+}
