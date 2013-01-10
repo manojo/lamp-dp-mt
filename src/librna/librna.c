308586c1088a1b53b1c4129728c4041602a22725
Thierry Coppey <tcknetwork@hotmail.com>
2013-01-10 12:47:36 +0100

    Nits.

--- src/librna/librna.c
+++ src/librna/librna.c
@@ -1,19 +1,56 @@
-#include "librna.h"
-#include "rnalib.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <jni.h>
+
+#include "rnalib.h"
+
+#define _fun(name) Java_librna_LibRNA_00024_##name
+#define _vfun(name) JNIEXPORT void JNICALL Java_librna_LibRNA_00024_##name
+#define _ifun(name) JNIEXPORT jint JNICALL Java_librna_LibRNA_00024_##name
+#define _p0 JNIEnv *env, jobject obj
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Definition of the classname, spread over all JNI functions
+_vfun(setParams)(_p0, jstring);
+_vfun(setSequence)(_p0, jstring);
+_vfun(clear)(_p0);
+_ifun(termau_1energy)(_p0, jint, jint);
+_ifun(hl_1energy)(_p0, jint, jint);
+_ifun(hl_1energy_1stem)(_p0, jint, jint);
+_ifun(il_1energy)(_p0, jint, jint, jint, jint);
+_ifun(bl_1energy)(_p0, jint, jint, jint, jint, jint);
+_ifun(br_1energy)(_p0, jint, jint, jint, jint, jint);
+_ifun(sr_1energy)(_p0, jint, jint);
+_ifun(sr_1pk_1energy)(_p0, jbyte, jbyte, jbyte, jbyte);
+_ifun(dl_1energy)(_p0, jint, jint);
+_ifun(dr_1energy)(_p0, jint, jint, jint);
+_ifun(dli_1energy)(_p0, jint, jint);
+_ifun(dri_1energy)(_p0, jint, jint);
+_ifun(ext_1mismatch_1energy)(_p0, jint, jint, jint);
+_ifun(ml_1mismatch_1energy)(_p0, jint, jint);
+_ifun(ml_1energy)(_p0);
+_ifun(ul_1energy)(_p0);
+_ifun(sbase_1energy)(_p0);
+_ifun(ss_1energy)(_p0, jint, jint);
+_ifun(dl_dangle_dg)(_p0, jbyte, jbyte, jbyte);
+_ifun(dr_dangle_dg)(_p0, jbyte, jbyte, jbyte);
+#ifdef __cplusplus
+}
+#endif
 
 static char* seq=NULL; // the sequence converted in 0-5 internal Vienna format
 
-void _fun(setParams)(JNIEnv *env, jobject obj, jstring file) {
+void _fun(setParams)(_p0, jstring file) {
   const char *str = (*env)->GetStringUTFChars(env, file, 0);
   librna_read_param_file(str);
   (*env)->ReleaseStringUTFChars(env, file, str);
 }
 
-void _fun(setSequence)(JNIEnv *env, jobject obj, jstring sequence) {
+void _fun(setSequence)(_p0, jstring sequence) {
   const char *str = (*env)->GetStringUTFChars(env, sequence, 0);
   size_t i,len = strlen(str);
   if (seq) free(seq);
@@ -32,25 +69,24 @@ void _fun(setSequence)(JNIEnv *env, jobject obj, jstring sequence) {
   (*env)->ReleaseStringUTFChars(env, sequence, str);
 }
 
-void _fun(clear)(JNIEnv *env, jobject obj) { if (seq) { free(seq); seq=NULL; } }
-
-jint _fun(termau_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return termau_energy(seq,i,j); }
-jint _fun(hl_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return hl_energy(seq,i,j); }
-jint _fun(hl_1energy_1stem)(JNIEnv *env, jobject obj, jint i, jint j) { return hl_energy_stem(seq,i,j); }
-jint _fun(il_1energy)(JNIEnv *env, jobject obj, jint i, jint k, jint l, jint j) { return il_energy(seq,i,k,l,j); }
-jint _fun(bl_1energy)(JNIEnv *env, jobject obj, jint i, jint k, jint l, jint j, jint Xright) { return bl_energy(seq,i,k,l,j,Xright); }
-jint _fun(br_1energy)(JNIEnv *env, jobject obj, jint i, jint k, jint l, jint j, jint Xleft) { return br_energy(seq,i,k,l,j,Xleft); }
-jint _fun(sr_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return sr_energy(seq,i,j); }
-jint _fun(sr_1pk_1energy)(JNIEnv *env, jobject obj, jbyte a, jbyte b, jbyte c, jbyte d) { return sr_pk_energy(a,b,c,d); }
-jint _fun(dl_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return dl_energy(seq,i,j); }
-jint _fun(dr_1energy)(JNIEnv *env, jobject obj, jint i, jint j, jint n) { return dr_energy(seq,i,j,n); }
-jint _fun(dli_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return dli_energy(seq,i,j); }
-jint _fun(dri_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return dri_energy(seq,i,j); }
-jint _fun(ext_1mismatch_1energy)(JNIEnv *env, jobject obj, jint i, jint j, jint n) { return ext_mismatch_energy(seq,i,j,n); }
-jint _fun(ml_1mismatch_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return ml_mismatch_energy(seq,i,j); }
-jint _fun(ml_1energy)(JNIEnv *env, jobject obj) { return ml_energy(); }
-jint _fun(ul_1energy)(JNIEnv *env, jobject obj) { return ul_energy(); }
-jint _fun(sbase_1energy)(JNIEnv *env, jobject obj) { return sbase_energy(); }
-jint _fun(ss_1energy)(JNIEnv *env, jobject obj, jint i, jint j) { return ss_energy(i,j); }
-jint _fun(dl_dangle_dg)(JNIEnv *env, jobject obj, jbyte dangle, jbyte i, jbyte j) { return dl_dangle_dg(dangle,i,j); }
-jint _fun(dr_dangle_dg)(JNIEnv *env, jobject obj, jbyte i, jbyte j, jbyte dangle) { return dl_dangle_dg(i,j,dangle); }
+void _fun(clear)(_p0) { if (seq) { free(seq); seq=NULL; } }
+jint _fun(termau_1energy)(_p0, jint i, jint j) { return termau_energy(seq,i,j); }
+jint _fun(hl_1energy)(_p0, jint i, jint j) { return hl_energy(seq,i,j); }
+jint _fun(hl_1energy_1stem)(_p0, jint i, jint j) { return hl_energy_stem(seq,i,j); }
+jint _fun(il_1energy)(_p0, jint i, jint k, jint l, jint j) { return il_energy(seq,i,k,l,j); }
+jint _fun(bl_1energy)(_p0, jint i, jint k, jint l, jint j, jint Xright) { return bl_energy(seq,i,k,l,j,Xright); }
+jint _fun(br_1energy)(_p0, jint i, jint k, jint l, jint j, jint Xleft) { return br_energy(seq,i,k,l,j,Xleft); }
+jint _fun(sr_1energy)(_p0, jint i, jint j) { return sr_energy(seq,i,j); }
+jint _fun(sr_1pk_1energy)(_p0, jbyte a, jbyte b, jbyte c, jbyte d) { return sr_pk_energy(a,b,c,d); }
+jint _fun(dl_1energy)(_p0, jint i, jint j) { return dl_energy(seq,i,j); }
+jint _fun(dr_1energy)(_p0, jint i, jint j, jint n) { return dr_energy(seq,i,j,n); }
+jint _fun(dli_1energy)(_p0, jint i, jint j) { return dli_energy(seq,i,j); }
+jint _fun(dri_1energy)(_p0, jint i, jint j) { return dri_energy(seq,i,j); }
+jint _fun(ext_1mismatch_1energy)(_p0, jint i, jint j, jint n) { return ext_mismatch_energy(seq,i,j,n); }
+jint _fun(ml_1mismatch_1energy)(_p0, jint i, jint j) { return ml_mismatch_energy(seq,i,j); }
+jint _fun(ml_1energy)(_p0) { return ml_energy(); }
+jint _fun(ul_1energy)(_p0) { return ul_energy(); }
+jint _fun(sbase_1energy)(_p0) { return sbase_energy(); }
+jint _fun(ss_1energy)(_p0, jint i, jint j) { return ss_energy(i,j); }
+jint _fun(dl_dangle_dg)(_p0, jbyte dangle, jbyte i, jbyte j) { return dl_dangle_dg(dangle,i,j); }
+jint _fun(dr_dangle_dg)(_p0, jbyte i, jbyte j, jbyte dangle) { return dl_dangle_dg(i,j,dangle); }
