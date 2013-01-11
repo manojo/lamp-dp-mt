6a747d5aee30919d940d2b3ee95f88de5e98684a
Manojo <manojo10386@gmail.com>
2013-01-11 13:20:14 +0100

    CFun generation from LMS

--- src/main/scala/lms/CGenTupleOps.scala
+++ src/main/scala/lms/CGenTupleOps.scala
@@ -18,33 +18,33 @@ trait CLikeGenTupleOps extends CLikeGenBase{
     case e2@ETuple2(a,b) =>
       registerType(sym.tp,rhs)
       emitValDef(sym, "("+tupleClassName(sym.tp)+"){"+ quote(a) + "," + quote(b) + "}")
-    case Tuple2Access1(t) => emitValDef(sym, quote(t) + ".t1")
-    case Tuple2Access2(t) => emitValDef(sym, quote(t) + ".t2")
+    case Tuple2Access1(t) => emitValDef(sym, quote(t) + "._1")
+    case Tuple2Access2(t) => emitValDef(sym, quote(t) + "._2")
 
     case e3@ETuple3(a,b,c) =>
       registerType(sym.tp,rhs)
       emitValDef(sym, "("+tupleClassName(sym.tp)+"){" + quote(a) + "," +quote(b) + "," + quote(c) + "}")
-    case Tuple3Access1(t) => emitValDef(sym, quote(t) + ".t1")
-    case Tuple3Access2(t) => emitValDef(sym, quote(t) + ".t2")
-    case Tuple3Access3(t) => emitValDef(sym, quote(t) + ".t3")
+    case Tuple3Access1(t) => emitValDef(sym, quote(t) + "._1")
+    case Tuple3Access2(t) => emitValDef(sym, quote(t) + "._2")
+    case Tuple3Access3(t) => emitValDef(sym, quote(t) + "._3")
 
     case e4@ETuple4(a,b,c,d) =>
       registerType(sym.tp,rhs)
       emitValDef(sym, "("+tupleClassName(sym.tp)+"){"+ quote(a) + "," + quote(b) + "," + quote(c) + "," + quote(d) + "}")
-    case Tuple4Access1(t) => emitValDef(sym, quote(t) + ".t1")
-    case Tuple4Access2(t) => emitValDef(sym, quote(t) + ".t2")
-    case Tuple4Access3(t) => emitValDef(sym, quote(t) + ".t3")
-    case Tuple4Access4(t) => emitValDef(sym, quote(t) + ".t4")
+    case Tuple4Access1(t) => emitValDef(sym, quote(t) + "._1")
+    case Tuple4Access2(t) => emitValDef(sym, quote(t) + "._2")
+    case Tuple4Access3(t) => emitValDef(sym, quote(t) + "._3")
+    case Tuple4Access4(t) => emitValDef(sym, quote(t) + "._4")
 
     case e5@ETuple5(a,b,c,d,e) =>
       registerType(sym.tp,rhs)
       emitValDef(sym, "("+tupleClassName(sym.tp)+"){"+ quote(a) + "," + quote(b) + "," + quote(c) + "," + quote(d) + ","
                          + quote(e) + "}")
-    case Tuple5Access1(t) => emitValDef(sym, quote(t) + ".t1")
-    case Tuple5Access2(t) => emitValDef(sym, quote(t) + ".t2")
-    case Tuple5Access3(t) => emitValDef(sym, quote(t) + ".t3")
-    case Tuple5Access4(t) => emitValDef(sym, quote(t) + ".t4")
-    case Tuple5Access5(t) => emitValDef(sym, quote(t) + ".t5")
+    case Tuple5Access1(t) => emitValDef(sym, quote(t) + "._1")
+    case Tuple5Access2(t) => emitValDef(sym, quote(t) + "._2")
+    case Tuple5Access3(t) => emitValDef(sym, quote(t) + "._3")
+    case Tuple5Access4(t) => emitValDef(sym, quote(t) + "._4")
+    case Tuple5Access5(t) => emitValDef(sym, quote(t) + "._5")
 
     case _ => super.emitNode(sym, rhs)
   }
@@ -78,11 +78,11 @@ trait CLikeGenTupleOps extends CLikeGenBase{
       for ((name, tuple) <- encounteredTuples) {
         stream.println{ tuple match {
           case e2@ETuple2(a,b) =>
-            "struct "+name+"{"+ remap(e2.m1) + " t1; " + remap(e2.m2) + " t2 }"
+            "struct "+name+"{"+ remap(e2.m1) + " t1; " + remap(e2.m2) + " t2; }"
           case e3@ETuple3(a,b,c) =>
-            "struct "+name+"{"+ remap(e3.m1) + " t1; " + remap(e3.m2) + " t2; "+ remap(e3.m3) +" t3 }"
+            "struct "+name+"{"+ remap(e3.m1) + " t1; " + remap(e3.m2) + " t2; "+ remap(e3.m3) +" t3; }"
           case e4@ETuple4(a,b,c,d) =>
-            "struct "+name+"{"+ remap(e4.m1) + " t1; " + remap(e4.m2) + " t2; "+ remap(e4.m3) +" t3; " + remap(e4.m4) +" t4 }"
+            "struct "+name+"{"+ remap(e4.m1) + " t1; " + remap(e4.m2) + " t2; "+ remap(e4.m3) +" t3; " + remap(e4.m4) +" t4; }"
           case e5@ETuple5(a,b,c,d,e) =>
             "struct "+name+"{"+ remap(e5.m1) + " t1; " + remap(e5.m2) + " t2; "+ remap(e5.m3) +" t3; " + remap(e5.m4) +" t4; " + remap(e5.m5) +" t5 }"
         }
@@ -93,3 +93,5 @@ trait CLikeGenTupleOps extends CLikeGenBase{
   }
 
 }
+
+trait CGenTupleOps extends CGenBase with CLikeGenTupleOps
