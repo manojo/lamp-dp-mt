1d5e5e9cc4587c7b7017dc397932fa23088dfc62
Thierry Coppey <tcknetwork@hotmail.com>
2013-01-15 22:09:43 +0100

    Nits.

---
+++ src/main/scala/v4/report/RNAFold.scala
@@ -0,0 +1,74 @@
+package v4.report
+import v4._
+
+trait RNAFoldSig extends RNASignature {
+  def hairpin(ij:(Int,Int)):Answer
+  def stack(i:Int,s:Answer,j:Int):Answer
+  def iloop(ik:(Int,Int),s:Answer,lj:(Int,Int)):Answer
+  def mloop(i:Int,s:Answer,j:Int):Answer
+  def left(l:Answer,r:Int):Answer
+  def right(l:Int,r:Answer):Answer
+  def join(l:Answer,r:Answer):Answer
+}
+
+trait RNAFoldAlgebra extends RNAFoldSig {
+  type Answer = Int
+  import librna.LibRNA._ // indexing convention: first base,last base
+  private def Eh(i:Int,j:Int) = hl_energy(i,j) // hairpin loop
+  private def Ei(i:Int,k:Int,l:Int,j:Int) = il_energy(i,k,l,j) // internal loop
+  private def Es(i:Int,j:Int) = sr_energy(i,j) // stacked base pairs
+  def hairpin(ij:(Int,Int)) = Eh(ij._1,ij._2-1)
+  def stack(i:Int,s:Int,j:Int) = Es(i,j) + s
+  def iloop(ik:(Int,Int),s:Int,lj:(Int,Int)) = Ei(ik._1,ik._2,lj._1-1,lj._2-1) + s
+  def mloop(i:Int,s:Int,j:Int) = s
+  def left(l:Int,r:Int) = l
+  def right(l:Int,r:Int) = r
+  def join(l:Int,r:Int) = l+r
+  override val h = min[Answer] _
+}
+
+trait RNAFoldPrettyPrint extends RNAFoldSig {
+  type Answer = String
+  private def dots(n:Int,c:Char='.') = (0 until n).map{_=>c}.mkString
+  def hairpin(ij:(Int,Int)) = "("+dots(ij._2-ij._1-2)+")"
+  def stack(i:Int,s:String,j:Int) = "("+s+")"
+  def iloop(ik:(Int,Int),s:String,lj:(Int,Int)) = "("+dots(ik._2-1-ik._1)+s+dots(lj._2-1-lj._1)+")"
+  def mloop(i:Int,s:String,j:Int) = "("+s+")"
+  def left(l:String,r:Int) = l+"."
+  def right(l:Int,r:String) = "."+r
+  def join(l:String,r:String) = l+r
+}
+
+trait RNAFoldGrammar extends ADPParsers with RNAFoldSig {
+  lazy val Qp:Tabulate = tabulate("Qp",(
+    seq(3,maxN)        ^^ hairpin
+  | eli   ~ Qp ~ eli   ^^ stack
+  | seq() ~ Qp ~ seq() ^^ iloop
+  | eli   ~ QM ~ eli   ^^ mloop
+  ) filter basepairing aggregate h)
+
+  lazy val QM:Tabulate = tabulate("QM",(Q ~ Q ^^ join) filter((i:Int,j:Int)=>i<=j+4) aggregate h)
+
+  lazy val Q:Tabulate = tabulate("Q",(
+    QM
+  | Q ~ eli ^^ left
+  | eli ~ Q ^^ right
+  | Qp
+  ) filter((i:Int,j:Int)=>i<=j+2) aggregate h)
+
+  override val axiom = Q
+}
+
+object RNAFold extends App {
+  object fold extends RNAFoldGrammar with RNAFoldAlgebra
+  object pretty extends RNAFoldGrammar with RNAFoldPrettyPrint
+
+  val seq="aaaaaagggaaaagaacaaaggagacucuucuccuuuuucaaaggaagaggagacucuuucaaaaaucccucuuuu"
+
+  import librna.LibRNA._
+  setSequence(seq)
+  val (score,bt) = fold.backtrack(seq.toArray).head
+  val res = pretty.build(seq.toArray,bt)
+  clear
+  println("Folding   : "+res+" (%5.2f)".format(score/100.0));
+}
