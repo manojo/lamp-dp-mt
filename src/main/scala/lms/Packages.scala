69c40f924cf5579292c379245596f4f8e82ba450
Thierry Coppey <tcknetwork@hotmail.com>
2013-03-06 15:41:59 +0100

    Packages.

---
+++ src/main/scala/lms/Packages.scala
@@ -0,0 +1,17 @@
+package lms
+
+trait Package extends ScalaOpsPkg with LiftScala {
+  
+}
+
+trait PackageExp extends ScalaOpsPkgExp {
+
+}
+
+trait ScalaGenPackage extends ScalaCodeGenPkg {
+
+}
+
+trait CGenPackage extends CCodeGenPkg {
+
+}
