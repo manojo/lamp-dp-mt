bbdf7489878ca44074d5fb99890fa7c22db1b898
Thierry Coppey <tcknetwork@hotmail.com>
2013-03-20 15:52:47 +0100

    Closer to MMAlpha code.

--- src/main/scala/v4/fpga/FPGACodeGen.scala
+++ src/main/scala/v4/fpga/FPGACodeGen.scala
@@ -83,7 +83,96 @@ trait FPGACodeGen extends CodeGen with TTParsers { this:Signature=>
   }
   */
 
-  override def genFun[T,U](f0:T=>U):String = "fun(<>)"
+/*
+  private val head = new CodeHeader(this) // User code and helpers store
+  override def genTab(t:Tabulate):String = {
+    var aggid=0; // nested aggregation intermediate result id
+    def scs(min:Int,max:Int,i:Var,j:Var) = if (min==max) List(i.eq(j,min)) else (if (max==maxN) Nil else List(j.leq(i,-max))):::(if (min>0) List(i.leq(j,min)) else Nil)
+    // Generate C parser for subword (i,j): collect conditions, hoisted/content and backtrack indices
+    // (parser, i,j,k?, subrule, aggregation_depth) => (Conditions+for loops, hoisted, body, backtrack indices)
+    def gen[T](p0:Parser[T],i:Var,j:Var,g:FreeVar,rule:Int,aggr:Int):(List[Cond],String,String,List[String]) = p0 match {
+      case Terminal(min,max,f) => val (cs,s)=f(i,j); (scs(min,max,i,j):::cs,"",s,Nil)
+      case p:Tabulate => ( (if (p.alwaysValid) Nil else List(CUser("VALID("+i+","+j+","+p.name+")"))):::scs(p.min,p.max,i,j),"","cost[idx("+i+","+j+")]."+p.name,Nil)
+      case Aggregate(p,h) => val (c,hb,b,bti)=gen(p,i,j,g,rule,aggr+1);
+        def bodyTpe:String = { // fallback to "typeof(body)" if untypable, where body has no fresh variable
+          val g0:FreeVar = new FreeVar('0') { override def get=zero; override def dup=this }
+          val (_,_,lb,_)=gen(p,zero,zero,g0,0,0); "typeof("+lb+")"
+        }
+        val tpe = (p,h) match {
+          case (_,MinBy(f:CFun)) => head.getType(f.args.head._2)
+          case (_,MaxBy(f:CFun)) => head.getType(f.args.head._2)
+          case (Map(_,f0),_) => val f1 = f0 match { case d:DeTuple => d.f case f => f }
+            f1 match { case f:CFun => head.getType(f.tpe) case _ => bodyTpe }
+          case _ => bodyTpe
+        }
+        val (tc,tb) = if(aggr==0) (if (cudaEmpty!=null) "_c0" else "_cost."+t.name,"_b0") else { aggid=aggid+1; ("_c"+aggid, "_b"+aggid) }
+        // Generate aggregation body
+        val cc = "{ "+(h.toString match {
+          case "$$max$$" => "Max("+b+", _c)"
+          case "$$min$$" => "Min("+b+", _c)"
+          case _ => sys.error("Unsupported aggregation: "+b)
+        })+" }"
+        if (aggr==0) (c,hb,cc,bti) // hoist aggregation if contained
+        else { val nv = tpe+" "+tc+"; "+btTpe(p.cat)+" "+tb+"={-1"+(if (p.cat>0)",{}" else "")+"};\n";
+          (List(CUser(tb+".rule!=-1")),nv+emit((c,hb,cc,bti)),tc,(0 until p.cat).map{x=>tb+".pos["+x+"]"}.toList)
+        }
+      case Or(l,r) => (Nil,"",emit(gen(l,i,j,g.dup,rule,aggr))+"\n"+emit(gen(r,i,j,g.dup,rule+l.alt,aggr)),Nil)
+      case Map(p,f) => val (c,hb,b,bti)=gen(p,i,j,g,rule,aggr); (c,hb,genFun(f)+"("+b+")",bti)
+      case Filter(p,f) => val (c,hb,b,bti)=gen(p,i,j,g,rule,aggr); (CUser(genFun(f)+"("+i+","+j+")")::c,hb,b,bti)
+      case cc@Concat(l,r,tt) =>
+        def bf1(f:Int, l:Int, u:Int):List[Cond] = { val ls=List(i.leq(j,f+l)); if (u!=maxN) j.leq(i,-f-u)::ls else ls }
+        val (c,k):(List[Cond],Var) = (tt,cc.indices) match {
+          // low=up in at least one side
+          case (0,(iL,iU,jL,jU)) if (iL==iU && jL==jU) => (List(i.eq(j,iL+jL)), i.add(iL))
+          case (0,(iL,iU,jL,jU)) if (iL==iU) => (bf1(iL,jL,jU), i.add(iL))
+          case (0,(iL,iU,jL,jU)) if (jL==jU) => (bf1(jL,iL,iU), j.add(-jL))
+          // most general case
+          case (0,(iL,iU,jL,jU)) => val k0=g.get; var cs:List[Cond]=Nil;
+            if (jU!=maxN) cs = j.leq(k0,-jU) :: cs
+            if (iU!=maxN) cs = k0.leq(i,-iU) :: cs // we might want to simplify if min_k==i || max_k==j
+            (CFor(k0.v,i.v,iL,j.v,jL)::cs, k0)
+          // concat1,concat2
+          case (t,(a,b,c,d)) if (t==1||t==2) => val (l,u,v) = if (t==1) (a,b,i) else (c,d,j)
+            if (l==u) (List(zero.leq(v,l)), v.add(-l))
+            else { val k0=g.get; val cu=if (u==maxN) Nil else List(k0.leq(v,u)); (CFor(k0.v,'0',0,v.v,l)::cu, k0) }
+        }
+        val (lc,lhb,lb,lbt) = if (tt==1) gen(l,k,i,g,rule,aggr) else gen(l,i,k,g,rule,aggr)
+        val (rc,rhb,rb,rbt) = gen(r,k,j,g,rule,aggr)
+        (simplify(c:::lc:::rc), lhb+(if (lhb!=""&&rhb!="") "\n" else "")+rhb, lb+","+rb, lbt:::(if(cc.hasBt) List(k.toString) else Nil):::rbt)
+      case _ => sys.error("Unknown parser")
+    }
+    // Generate the loops and size conditions (conditions, hoisted, body, indices)
+    def emit(cb:(List[Cond],String,String,List[String])):String = cb._3
+    // Generate the whole tabulation
+    emit(gen(norm(Aggregate(t.inner,h)),Var('i',0),Var('j',0),new FreeVar('k'),t.id,0))
+  }
+*/
+  var ctr=0;
+  override def genFun[T,U](f0:T=>U):String = { val f1 = f0 match { case d:DeTuple => d.f case f => f }
+    f1 match { case f:CFun => ctr=ctr+1; println("fun"+ctr+" = ("+f.args.map{_._1}.mkString(",")+") => "+f.body); "fun"+ctr case _ => sys.error("Unsupported function: "+f1) }
+  }
+
+  def genParser[T](p0:Parser[T],i:Var,j:Var):List[String] = p0 match {
+    case Aggregate(p,h) => 
+      val f = h.toString match { case "$$max$$"=>"max" case "$$min$$"=>"min" case _=>sys.error("Unsupported aggregation: "+h) }
+      val l = genParser(p,i,j); List((l.head /: l.tail){(a,b)=>"max("+a+","+b+")"  })
+    case Or(l,r) => genParser(l,i,j):::genParser(r,i,j)
+    case Map(p,f) => List(genFun(f)+"("+genParser(p,i,j).mkString(",")+")")
+    case cc@Concat(l,r,t) =>
+        val k:Var = (t,cc.indices) match { // only support serial dependencies
+          case (0,(l,u,_,_)) if (l==u) => i.add(l)
+          case (0,(_,_,l,u)) if (l==u) => j.add(-l)
+          case (1,(l,u,_,_)) if (l==u) => i.add(-l)
+          case (2,(_,_,l,u)) if (l==u) => j.add(-l)
+          case _ => sys.error("Unsupported non-serial dependencies")
+        }
+        List((if (t==1) genParser(l,k,i) else genParser(l,i,k)).head,genParser(r,k,j).head)
+    case `el1` => List("s1["+i+"]")
+    case `el2` => List("s2["+i+"]")
+    case `empty` => List("")
+    case t:Tabulate => List(t.name+"["+i+","+j+"]")
+    case _ => sys.error("Unsupported parser "+p0)
+  }
 
   override def gen:String = {
     analyze; val rs=rulesOrder.map{n=>rules(n)}
@@ -100,7 +189,8 @@ trait FPGACodeGen extends CodeGen with TTParsers { this:Signature=>
       print("; j >= " + jx(j) + (if(j == jx.length -1) "" else "; j < " + jx(j+1)))
       print("} : ")
       getRulesFor(axiom.inner, ix(i), jx(j)) match {
-        case Some(p) => println(genTab(tabulate("tab"+i+"_"+j,p))) // XXX: use a Tabulation -> Parser transform => XXX: use custom codegen instead
+        case Some(p) => println(genParser(p,Var('i',0),Var('j',0)).head)
+        //case Some(p) => println(genTab(tabulate("tab"+i+"_"+j,p))) // XXX: use a Tabulation -> Parser transform => XXX: use custom codegen instead
         case _ => sys.error("No parser applies")
       }
       //println(getRulesFor(axiom, ix(i), jx(j)))
