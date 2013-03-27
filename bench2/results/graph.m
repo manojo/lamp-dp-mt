a9fd3b6b67c4861f5cd4b45fe326a935dc06eba7
Thierry Coppey <tcknetwork@hotmail.com>
2013-03-27 18:40:53 +0100

    Nits.

--- bench2/results/graph.m
+++ bench2/results/graph.m
@@ -1,28 +1,22 @@
 
 function [] = mbp()
-
 	[D, axis, D2, axis2] = data()
 
 	% D=[1=vienna; 2=adp; 3=lms; 4-7=cpu(NuPlain,NuBT, ZuPlain, ZuBT); 8-11=gpu(NuPlain,NuBT, ZuPlain, ZuBT)];
-
-	do_graph('zuker.eps','Zuker',D,axis,[1,2,6,7,10,11],{'-k','--k','-rd','-gx','-bs','-mo'},{'ViennaRNA','ADP fusion','CPU','CPU+BT','GPU','GPU+BT'})
-	do_graph('nussinov.eps','Nussinov',D,axis,[3,4,5,8,9],{'-k','-rd','-gx','-bs','-mo'},{'LMS','CPU','CPU+BT','GPU','GPU+BT'})
+	do_graph('small_zu.eps','Zuker',D,axis,[1,2,6,7,10,11],{'-k','--k','-rd','-gx','-bs','-mo'},{'ViennaRNA','ADP fusion','CPU','CPU+BT','GPU','GPU+BT'})
+	do_graph('small_nu.eps','Nussinov',D,axis,[3,4,5,8,9],{'-k','-rd','-gx','-bs','-mo'},{'LMS','CPU','CPU+BT','GPU','GPU+BT'})
 
 	% D2 = [ 1-4=cpu(NuPlain,NuBT, ZuPlain, ZuBT); 5-8=gpu(NuPlain,NuBT, ZuPlain, ZuBT) ]
-
 	do_graph('large_nu.eps','Nussinov',D2,axis2,[1,2,5,6],{'-rd','-gx','-bs','-mo'},{'CPU','CPU+BT','GPU','GPU+BT'})
 	do_graph('large_zu.eps','Zuker',D2,axis2,[3,4,7,8],{'-rd','-gx','-bs','-mo'},{'CPU','CPU+BT','GPU','GPU+BT'})
 
-	%subplot(121);
-	%subplot(122);
 end
 
 % specs: http://www.mathworks.ch/ch/help/matlab/ref/linespec.html
 function do_graph(file,name,D,axis,lines,specs,labels)
 	h=figure;
 	hh=[];
-
-	cc = hsv(length(lines));
+	%cc = hsv(length(lines));
 	for i=1:length(lines),
 		hh(end+1)=semilogy(axis,D(lines(i),:),specs{i},'linewidth',1.5,'markersize',8);
 		hold on;
