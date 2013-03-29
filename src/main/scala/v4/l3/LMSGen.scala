b23f3ee602c9d1185f13ab13ea24770210893395
Thierry Coppey <tcknetwork@hotmail.com>
2013-03-29 17:43:14 +0100

    Compiling LMS-Scala code (but still not tested).

--- src/main/scala/v4/l3/LMSGen.scala
+++ src/main/scala/v4/l3/LMSGen.scala
@@ -10,116 +10,19 @@ object Test extends Signature with LMSGenADP with App {
   type Answer = (Int,Int,Int) // col
   val tps=(manifest[Alphabet],manifest[Answer])
 
-  override val h = minBy(toFun{ (a:Rep[Answer]) => a._2  })
-  val single = toFun{(a: Rep[Alphabet]) => (a._1, unit(0), a._2)} 
-  val mult = toFun{p:Rep[(Answer,Answer)] => { val l=p._1; val r=p._2; (l._1, l._2 + r._2 + l._1 * l._3 * r._3, r._3) } }
+  override val h = minBy(lfun{ (a:Rep[Answer]) => a._2  })
+  val single = lfun{(a: Rep[Alphabet]) => (a._1, unit(0), a._2)} 
+  val mult = lfun{p:Rep[(Answer,Answer)] => { val l=p._1; val r=p._2; (l._1, l._2 + r._2 + l._1 * l._3 * r._3, r._3) } }
   val axiom:Tabulate = tabulate("M",(el ^^ single | axiom ~ axiom ^^ mult) aggregate h)
 
-  scala.Console.println(parse(scala.Array((3,2),(2,4),(4,2),(2,5)),psBottomUp))
+  val mats = scala.Array((1,2),(2,20),(20,2),(2,4),(4,2),(2,1),(1,7),(7,3)) // -> 1x3 matrix, 122 multiplications
 
-  scala.Console.println(genLMS)
-}
+  // Vanilla Scala version
+  scala.Console.println(parse(mats,psBottomUp))
 
-/*
-BOILS DOWN TO CORRECT CODE (SIMPLIFIED):
-class kernel extends ((Int, Int)=>(Unit)) {
-  def apply(x0:Int, x1:Int): Unit = {
-    var x2: Int = -1
-    val x3 = new Array[Int](1);
-    val x4 = new Array[Int](1);
-    var x5: scala.Tuple3[Int, Int, Int] = null
-    val x6 = x0 + 1
-    val x7 = x6 == x1
-    val x26 = if (x7) {
-      val x8 = _in1[x0];
-      if (x2 == -1 || 0 < x5._2) {
-        x5 = (x8._1,0,x8._2)
-        x2 = 0
-        x3 = x4.clone();
-      }
-    }
-    val x30 = x6
-    var x38 : Int = x30
-    while (x38 < x1) {
-      x4(0) = x38;
-      val x41 = _cost.M[idx(x0,x38)];
-      val x43 = _cost.M[idx(x38,x1)];
-      val x50 = x43._3
-      val x45 = x41._1
-      val x54 = x41._2 + x43._2 + x45 * x41._3 * x50
-      if (x2 == -1 || x54 < x5._2) {
-        x5 = (x45,x54,x50)
-        x2 = 0
-        x3 = x4.clone();
-      }
-      x38 = x38 + 1
-    }
-    val x73 = idx(x0,x1);
-    _cost.M[x73] = x5;
-    _back.M[x73].rule = x2;
-    _back.M[x73].pos = x3;
-  }
-}
-*/
-
-// -----------------------------------------------------------------
-
-import scala.virtualization.lms.common._
-import scala.reflect.SourceContext
-import scala.virtualization.lms.internal.{Expressions,Effects}
-
-trait DPOps extends Base with Expressions with Effects {
-  def tab_idx(i:Rep[Int], j:Rep[Int])(implicit pos: SourceContext): Rep[Int] // to get common subexpression elimination
-  def tab_read[T](name:String, idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext): Rep[T]
-  def tab_write[T:Manifest](name:String, idx:Rep[Int], value:Rep[T], rule:Rep[Int], bt:Rep[Array[Int]])(implicit pos: SourceContext): Rep[Unit]
-  def in1_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext): Rep[T]
-  def in2_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext): Rep[T]
-
-  // int[] stack array storage for backtrack
-  def bt_new(size:Int)(implicit pos: SourceContext) : Rep[Array[Int]]
-  def bt_set(dst:Rep[Array[Int]], idx:Int, value:Rep[Int])(implicit pos: SourceContext) : Rep[Unit]
-  def bt_copy(dst:Rep[Array[Int]],src:Rep[Array[Int]])(implicit pos: SourceContext) : Rep[Unit]
-}
-
-trait DPExp extends DPOps with EffectExp {
-  case class TabIdx(i:Exp[Int], j:Exp[Int])(implicit pos: SourceContext) extends Def[Int]
-  case class TabRead[T:Manifest](name:String, idx:Exp[Int])(implicit pos: SourceContext) extends Def[T]
-  case class TabWrite[T:Manifest](name:String, idx:Exp[Int], value:Exp[T], rule:Rep[Int], bt:Exp[Array[Int]])(implicit pos: SourceContext) extends Def[Unit]
-  case class In1Read[T:Manifest](idx:Exp[Int])(implicit pos: SourceContext) extends Def[T]
-  case class In2Read[T:Manifest](idx:Exp[Int])(implicit pos: SourceContext) extends Def[T]
-
-  case class BtNew(size:Int)(implicit pos: SourceContext) extends Def[Array[Int]]
-  case class BtSet(dst:Exp[Array[Int]], idx:Int, value:Exp[Int]) extends Def[Unit]
-  case class BtCopy(dst:Exp[Array[Int]], src:Exp[Array[Int]]) extends Def[Unit]
-
-  def tab_idx(i:Rep[Int], j:Rep[Int])(implicit pos: SourceContext) = TabIdx(i,j)
-  def tab_read[T](name:String, idx:Exp[Int])(mT:Manifest[T])(implicit pos: SourceContext) = reflectEffect(TabRead(name,idx))
-  def tab_write[T:Manifest](name:String, idx:Rep[Int], value:Rep[T], rule:Rep[Int], bt:Rep[Array[Int]])(implicit pos: SourceContext) = reflectEffect(TabWrite(name,idx,value,rule,bt))
-  def in1_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext) = reflectEffect(In1Read(idx))
-  def in2_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext) = reflectEffect(In2Read(idx))
-
-  def bt_new(size:Int)(implicit pos: SourceContext) = reflectEffect(BtNew(size))
-  def bt_set(dst:Rep[Array[Int]], idx:Int, value:Rep[Int])(implicit pos: SourceContext) = reflectEffect(BtSet(dst,idx,value))
-  def bt_copy(dst:Rep[Array[Int]],src:Rep[Array[Int]])(implicit pos: SourceContext) = reflectEffect(BtCopy(dst,src))
-}
-
-trait ScalaGenDP extends ScalaGenEffect {
-  val IR: DPExp
-  import IR._
-  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
-    case TabIdx(i,j) => stream.println("val "+quote(sym)+" = idx("+quote(i)+","+quote(j)+");")
-    case TabRead(name,idx) => stream.println("val "+quote(sym)+" = _cost."+name+"["+quote(idx)+"];")
-    case TabWrite(name,idx,value,rule,bt) =>
-      stream.println("_cost."+name+"["+quote(idx)+"] = "+quote(value)+";")
-      stream.println("_back."+name+"["+quote(idx)+"].rule = "+quote(rule)+";")
-      stream.println("_back."+name+"["+quote(idx)+"].pos = "+quote(bt)+";")
-    case In1Read(i) => stream.println("val "+quote(sym)+" = _in1["+quote(i)+"];")
-    case In2Read(i) => stream.println("val "+quote(sym)+" = _in2["+quote(i)+"];")
-    case BtNew(size) => stream.println("val "+quote(sym)+" = new Array[Int]("+size+");")
-    case BtSet(dst, idx, value) => stream.println(quote(dst)+"("+idx+") = "+quote(value)+";")
-    case BtCopy(dst, src) => stream.println(quote(dst)+" = "+quote(src)+".clone();")
-    case _ => super.emitNode(sym, rhs)
-  }
+  // LMS generated code
+  scala.Console.println(parseLMS(mats))
+  //scala.Console.println(genLMS)
 }
 
 // -----------------------------------------------------------------
@@ -131,7 +34,7 @@ trait LMSGenADP extends ADPParsers with LMSGen { self:Signature =>
     case `emptyi` => if (i==j) cont(i.asInstanceOf[Rep[T]])
     case `el` => if (i+1==j) cont(in(i).asInstanceOf[Rep[T]])
     case `eli` => if (i+1==j) cont(i.asInstanceOf[Rep[T]])
-    // XXX: missing seq(min,max) => create a class and pattern match
+    // XXX: missing seq(min,max) => create a case class to pattern match
     case _ => sys.error("Unsupported terminal")
   }
 }
@@ -142,20 +45,26 @@ trait LMSGenADP extends ADPParsers with LMSGen { self:Signature =>
 
 // -----------------------------------------------------------------
 
-trait LMSGen extends CodeGen with ScalaOpsPkgExp with GeneratorOpsExp with DPExp with BooleanOpsExp { self:Signature =>
+trait LMSGen extends CodeGen with ScalaOpsPkgExp with GeneratorOpsExp with DPExp with BooleanOpsExp with ADPParsers { self:Signature =>
   val codegen = new ScalaCodeGenPkg with ScalaGenGeneratorOps with ScalaGenDP { val IR:self.type = self }
+  //val ccodegen = new CCodeGenPkg with CGenGeneratorOps with CGenDP { val IR:self.type = self }
 
-  // Replace LMS's ScalaCompiler
+  // Replaces LMS's ScalaCompiler
   private var compileCount = 0
+  private def freshClassName = { compileCount=compileCount+1; "staged$"+compileCount }
+
+
   def compile[A,B](f: Exp[A] => Exp[B])(implicit mA: Manifest[A], mB: Manifest[B]): A=>B = {
-    val className = "staged$" + compileCount
+    val className = freshClassName
     val source = new java.io.StringWriter()
     codegen.emitSource(f, className, new java.io.PrintWriter(source))
     compiler.compile[A=>B](className,source.toString)
   }
 
+
+
   // Helper to wrap LMS function appropriately
-  def toFun[T:Manifest,U:Manifest](f:Rep[T]=>Rep[U]) = new LFun(f)
+  def lfun[T:Manifest,U:Manifest](f:Rep[T]=>Rep[U]) = new LFun(f)
   case class LFun[T:Manifest,U:Manifest](f:Rep[T]=>Rep[U]) extends Function1[T,U] {
     //val mT = manifest[T] // to feed at LMS node construction
     //val mU = manifest[U]
@@ -169,6 +78,52 @@ trait LMSGen extends CodeGen with ScalaOpsPkgExp with GeneratorOpsExp with DPExp
   private def _read(t:Tabulate,i:Rep[Int],j:Rep[Int]) = tab_read(t.name,tab_idx(i,j))(tps._2)
   private def _write(t:Tabulate,i:Rep[Int],j:Rep[Int],value:Rep[Answer],rule:Rep[Int],bt:Rep[Array[Int]]) = { implicit val manifestForAnswer=tps._2; tab_write(t.name,tab_idx(i,j),value,rule,bt) }
   
+
+  def parseLMS(in1:Input):List[(Answer,Trace)] = {
+    val buf = new java.io.ByteArrayOutputStream;
+    val pr = new java.io.PrintWriter(buf)
+    analyze
+    implicit val manifestForAlphabet=tps._1
+    implicit val manifestForAnswer=tps._2
+
+    val className = freshClassName
+    val source = new java.io.StringWriter()
+
+    codegen.emitSource2(bottomUpLMS _, className, new java.io.PrintWriter(source))
+    scala.Console.println("SOURCE CODE = "+source.toString)
+    val bu = compiler.compile[(Input,List[ Array[(Answer,Backtrack)] ]) =>Unit](className,source.toString)
+
+/*
+    input=in1; analyze; tabInit(in1.size+1,in1.size+1);
+
+    // Bottom-up application
+    tabReset; input=null; res
+*/
+    Nil
+  }
+
+
+  // XXX: also make a wrapper that alloc/free tables and does the backtrack
+  def bottomUpLMS(in1:Rep[Input], /*in2:Rep[Input],*/ tabs: Rep[List[ Array[(Answer,Backtrack)] ]]) : Rep[Unit] = {
+    implicit val manifestForAlphabet=tps._1
+    implicit val manifestForAnswer=tps._2
+
+    named_val("in1",in1)
+    named_val("size1",in1.length+unit(1))
+    // named_val("input_2",in2)
+    rulesOrder.zipWithIndex.foreach{case (n,i)=>
+      named_val("tab_"+n,tabs(unit(i)))
+    }
+    // explicitly name tables
+    range_foreach(range_until(unit(0),in1.length+unit(1)),(l:Rep[Int])=>{
+      range_foreach(range_until(unit(0),in1.length+unit(1)),(i:Rep[Int])=>{ val j=i+l
+        rulesOrder.foreach{n=>
+          genTabLMS(rules(n))(i,j)
+        } // XXX: address the rule names in codegen
+      })
+    })
+  }
+
   def genTerminal[T](t:Terminal[T],i:Rep[Int],j:Rep[Int],cont:Rep[T]=>Rep[Unit]) : Rep[Unit] // abstract
 
   def genTabLMS(t:Tabulate)(i0:Rep[Int],j0:Rep[Int]):Rep[Unit] = {
@@ -243,3 +198,90 @@ trait LMSGen extends CodeGen with ScalaOpsPkgExp with GeneratorOpsExp with DPExp
     buf.toString
   }
 }
+
+// -----------------------------------------------------------------
+
+import scala.virtualization.lms.common._
+import scala.reflect.SourceContext
+import scala.virtualization.lms.internal.{Expressions,Effects}
+
+trait DPOps extends Base with Expressions with Effects {
+  def tab_idx(i:Rep[Int], j:Rep[Int])(implicit pos: SourceContext): Rep[Int] // to get common subexpression elimination
+  def tab_read[T](name:String, idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext): Rep[T]
+  def tab_write[T:Manifest](name:String, idx:Rep[Int], value:Rep[T], rule:Rep[Int], bt:Rep[Array[Int]])(implicit pos: SourceContext): Rep[Unit]
+  def in1_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext): Rep[T]
+  def in2_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext): Rep[T]
+
+  // int[] stack array storage for backtrack
+  def bt_new(size:Int)(implicit pos: SourceContext) : Rep[Array[Int]]
+  def bt_set(dst:Rep[Array[Int]], idx:Int, value:Rep[Int])(implicit pos: SourceContext) : Rep[Unit]
+  def bt_copy(dst:Rep[Array[Int]],src:Rep[Array[Int]])(implicit pos: SourceContext) : Rep[Unit]
+
+  def named_val[T](name:String,value:Rep[T]) : Rep[Unit]
+}
+
+trait DPExp extends DPOps with EffectExp {
+  case class TabIdx(i:Exp[Int], j:Exp[Int])(implicit pos: SourceContext) extends Def[Int]
+  case class TabRead[T:Manifest](name:String, idx:Exp[Int])(implicit pos: SourceContext) extends Def[T]
+  case class TabWrite[T:Manifest](name:String, idx:Exp[Int], value:Exp[T], rule:Rep[Int], bt:Exp[Array[Int]])(implicit pos: SourceContext) extends Def[Unit]
+  case class In1Read[T:Manifest](idx:Exp[Int])(implicit pos: SourceContext) extends Def[T]
+  case class In2Read[T:Manifest](idx:Exp[Int])(implicit pos: SourceContext) extends Def[T]
+
+  case class BtNew(size:Int)(implicit pos: SourceContext) extends Def[Array[Int]]
+  case class BtSet(dst:Exp[Array[Int]], idx:Int, value:Exp[Int]) extends Def[Unit]
+  case class BtCopy(dst:Exp[Array[Int]], src:Exp[Array[Int]]) extends Def[Unit]
+
+  case class NamedVal[T](name:String,value:Rep[T]) extends Def[Unit]
+
+  def tab_idx(i:Rep[Int], j:Rep[Int])(implicit pos: SourceContext) = TabIdx(i,j)
+  def tab_read[T](name:String, idx:Exp[Int])(mT:Manifest[T])(implicit pos: SourceContext) = reflectEffect(TabRead(name,idx))
+  def tab_write[T:Manifest](name:String, idx:Rep[Int], value:Rep[T], rule:Rep[Int], bt:Rep[Array[Int]])(implicit pos: SourceContext) = reflectEffect(TabWrite(name,idx,value,rule,bt))
+  def in1_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext) = reflectEffect(In1Read(idx))
+  def in2_read[T](idx:Rep[Int])(mT:Manifest[T])(implicit pos: SourceContext) = reflectEffect(In2Read(idx))
+
+  def bt_new(size:Int)(implicit pos: SourceContext) = reflectEffect(BtNew(size))
+  def bt_set(dst:Rep[Array[Int]], idx:Int, value:Rep[Int])(implicit pos: SourceContext) = reflectEffect(BtSet(dst,idx,value))
+  def bt_copy(dst:Rep[Array[Int]],src:Rep[Array[Int]])(implicit pos: SourceContext) = reflectEffect(BtCopy(dst,src))
+
+  def named_val[T](name:String,value:Rep[T]) = reflectEffect(NamedVal(name,value))
+}
+
+trait ScalaGenDP extends ScalaGenEffect {
+  val IR: DPExp
+  import IR._
+  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+    // XXX: store into single cell arrays instead of Array[List[...]]
+    case TabIdx(i,j) => stream.println("val "+quote(sym)+" = "+quote(i)+" * size1 + "+quote(j)) // XXX: fix dimension
+    case TabRead(name,idx) => stream.println("val "+quote(sym)+" = tab_"+name+"("+quote(idx)+")._1")
+    case TabWrite(name,idx,value,rule,bt) => stream.println("tab_"+name+"("+quote(idx)+") = ("+quote(value)+",("+quote(rule)+","+quote(bt)+".toList)); val "+quote(sym)+"=()")
+    case In1Read(i) => stream.println("val "+quote(sym)+" = in1("+quote(i)+");")
+    case In2Read(i) => stream.println("val "+quote(sym)+" = in2("+quote(i)+");")
+    case BtNew(size) => stream.println("var "+quote(sym)+" = new Array[Int]("+size+");")
+    case BtSet(dst, idx, value) => stream.println(quote(dst)+"("+idx+") = "+quote(value)+"; val "+quote(sym)+"=()")
+    case BtCopy(dst, src) => stream.println(quote(dst)+" = "+quote(src)+".clone(); val "+quote(sym)+"=()")
+    case NamedVal(name,value) => stream.println("val "+name+" = "+quote(value)+";")
+    case _ => super.emitNode(sym, rhs)
+  }
+}
+
+/*
+trait CGenDP extends CGenEffect {
+  val IR: DPExp
+  import IR._
+  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
+    case TabIdx(i,j) => stream.println("val "+quote(sym)+" = idx("+quote(i)+","+quote(j)+");")
+    case TabRead(name,idx) => stream.println("val "+quote(sym)+" = _cost."+name+"["+quote(idx)+"];")
+    case TabWrite(name,idx,value,rule,bt) =>
+      stream.println("_cost."+name+"["+quote(idx)+"] = "+quote(value)+";")
+      stream.println("_back."+name+"["+quote(idx)+"].rule = "+quote(rule)+";")
+      stream.println("_back."+name+"["+quote(idx)+"].pos = "+quote(bt)+";")
+    case In1Read(i) => stream.println("val "+quote(sym)+" = _in1["+quote(i)+"];")
+    case In2Read(i) => stream.println("val "+quote(sym)+" = _in2["+quote(i)+"];")
+    case BtNew(size) => stream.println("val "+quote(sym)+" = new Array[Int]("+size+");")
+    case BtSet(dst, idx, value) => stream.println(quote(dst)+"("+idx+") = "+quote(value)+";")
+    case BtCopy(dst, src) => stream.println(quote(dst)+" = "+quote(src)+".clone();")
+    case NamedVal(name,value) => stream.println("val n_"+name+" = "+quote(value)+";")
+    case _ => super.emitNode(sym, rhs)
+  }
+}
+*/
